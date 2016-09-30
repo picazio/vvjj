8d1d58b4448f836a86ab132785747d324fd1d51c
picazio <attilio.picazio@gmail.com>
2016-09-30 17:12:46 +0200

    fixes for more pValues

--- pValueMacro/runPvaluePlotter.py
+++ pValueMacro/runPvaluePlotter.py
@@ -10,17 +10,23 @@
 
 ############################# Settings area  ############################
 
-runOverWS= True #Set to 'False' if you already have the output*.txt files
-inputPreDir="/afs/cern.ch/work/y/yygao/public/VVJJWorkspaces/28Sep2016/"
+runOverWS= False #Set to 'False' if you already have the output*.txt files
+#inputPreDir="/afs/cern.ch/work/y/yygao/public/VVJJWorkspaces/28Sep2016/"
 #inputName = "CxAOD_23Sep2016_13p3ifb_NoTrk"
-inputName = "CxAOD_23Sep2016_13p3ifb"
-lumitag = "13p3"
+#inputName = "CxAOD_23Sep2016_13p3ifb"
+#lumitag = "13p3"
+
+inputPreDir="/afs/cern.ch/work/y/yygao/public/VVJJWorkspaces/28July2016/"
+#inputName = "CxAOD_23Sep2016_13p3ifb_NoTrk"
+inputName = "CxAOD_28July2016_15p5ifb_nominal"
+lumitag = "15p5"
+
 #lumitag = ""
 models_dic={
-    "HVT":["WZ",1,2,1,"HVT W' #rightarrow WZ"],
-    #"HVT":["WW",4,2,2,"HVT Z' #rightarrow WW"],
-    #"Gravi":["WW",7,2,3,"G_{RS} #rightarrow WW"],
-    #"Gravi":["ZZ",6,2,4,"G_{RS} #rightarrow ZZ"],
+    "HVTWZ":["HVT","WZ",1,2,1,"HVT W' #rightarrow WZ"],
+    "HVTWW":["HVT","WW",4,2,2,"HVT Z' #rightarrow WW"],
+    "GraviWW":["Gravi","WW",7,2,3,"G_{RS} #rightarrow WW"],
+    "GraviZZ":["Gravi","ZZ",6,2,4,"G_{RS} #rightarrow ZZ"],
     }
 hasNtrk = True
 pValuePlotName="pValueAll.pdf"
@@ -44,12 +50,14 @@ for imodeldic, modeldic in enumerate(model_dic_key) :
 
     cpCommand="cp "+nameMacro+" "+tempRunningMacro
     os.system(cpCommand)
-    functions.replaceArguments(tempRunningMacro,inputPreDir,inputName,lumitag,modeldic,models_dic[modeldic][0],hasNtrk)
+    functions.replaceArguments(tempRunningMacro,inputPreDir,inputName,lumitag,models_dic[modeldic][0],models_dic[modeldic][1],hasNtrk)
     runCommand="root -l -b -q calcPvalue.C"
     if runOverWS :
         os.system(runCommand)
-    outPutName="output_p0_"+inputName+"_"+modeldic+"_"+models_dic[modeldic][0]+".txt"
+    outPutName="output_p0_"+inputName+"_"+models_dic[modeldic][0]+"_"+models_dic[modeldic][1]+".txt"
     models_dic[modeldic].append(outPutName)
 
+
+    
 functions.readOutputFilePValue(models_dic)
 functions.createPValuePlot(models_dic,pValuePlotName,'.'.join(lumitag.split('p'))+" fb^{-1}")
